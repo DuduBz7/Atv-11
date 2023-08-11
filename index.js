74b2a1451f4b238f3495d26f7a3aa85cd6f6a3e9
Luiz Eduardo Oliveira Mendes <132697970+DuduBz7@users.noreply.github.com>
2023-08-11 13:06:20 +0000

    Salvando

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
 
-//index.js
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -25,6 +25,21 @@ app.get("/usuarios", async (req, res) => {
   try {
     const usuarios = await selectUsuarios();
     res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+
+import { selectUsuarios, selectUsuario } from "./bd.js";
+
+
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
